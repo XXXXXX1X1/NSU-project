f71795959ef62c5852ac4b2224c87fda98f9f7e8
Salapanov Alexander <salapanov20006@gmail.com>
2025-09-10 21:18:22 +0700

    Обновил main.py и text.txt

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import torch
 from transformers import pipeline, AutoTokenizer
 
-model_id = "facebook/mbart-large-50-many-to-many-mmt"
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
+MODEL = "csebuetnlp/mT5_multilingual_XLSum"
 
+# выбираем GPU, если он доступен
 device = 0 if torch.cuda.is_available() else -1
+
+# токенайзер без предупреждений
+tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=False, legacy=False)
 
 summarizer = pipeline(
     "summarization",
-    model=model_id,
+    model=MODEL,
     tokenizer=tokenizer,
     device=device
 )
 
-# Читаем текст из файла
 with open("text.txt", "r", encoding="utf-8") as f:
     text = f.read().strip()
 
-# Генерация заголовка
-result = summarizer(text, max_length=15, min_length=5, do_sample=False, num_beams=4)
-headline = result[0]["summary_text"]
+result = summarizer(
+    text,
+    max_new_tokens=15,
+    min_length=5,
+    do_sample=False,
+    num_beams=8,
+    no_repeat_ngram_size=3,
+    length_penalty=1.2
+)
 
+headline = result[0]["summary_text"].strip()
 print("Заголовок:", headline)
